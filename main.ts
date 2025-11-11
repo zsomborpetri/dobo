3abf2171c8fad003eff65e45a0ac652967af9e06
zsomborpetri <126389789+zsomborpetri@users.noreply.github.com>
2025-11-11 14:09:18 +0100

    első verzió

--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
-
+let szam = 0
+input.onButtonPressed(Button.A, function () {
+    // Dobókocka animáció (forgás imitáció)
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+        basic.pause(100)
+    }
+    // Véletlenszám generálása
+    szam = randint(1, 6)
+    // Szám megjelenítése
+    basic.showNumber(szam)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
